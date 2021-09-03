2ac3aed94c98f354047f933063c59009c5542e7f
Guilherme Goncalves <33465712+guuilog@users.noreply.github.com>
2021-09-03 10:19:28 -0300

    Add files via upload

---
+++ URL.js
@@ -0,0 +1,9 @@
+ (function(){
+              try{
+                var a = document.location.pathname.split("/");
+            var num = a[1];
+            var b = '';
+            b += num;          
+                return b;
+        }
+            catch(a){}}())
